49e61905bdc10e97772238a76152f989a1fafeda
markus-cyborg <dekeynkustan@gmail.com>
2025-01-11 08:57:17 +0700

    Update app.py and requirements.txt

--- app.py
+++ app.py
@@ -1,51 +1,29 @@
-from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
 import mysql.connector
 from mysql.connector import Error
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-# Konfigurasi database MariaDB untuk SQLAlchemy
+# Konfigurasi database MariaDB dengan kredensial baru
+hostname = "2vgwh.h.filess.io"
+database = "Responsi_scaredyard"
+port = "3305"
+username = "Responsi_scaredyard"
+password = "b659a8bd3ea2b44fd40a57b3e51d85bbabc7ebc4"
+
+# Konfigurasi SQLAlchemy untuk Flask
 app.config['SQLALCHEMY_DATABASE_URI'] = (
-    'mysql+pymysql://Responsi0972_rubberdark:209fd4754cb6b52c80b773bdb355fdaf320010f1'
-    '@2wxsc.h.filess.io:3305/Responsi0972_rubberdark'
+    f'mysql+mysqlconnector://{username}:{password}@{hostname}:{port}/{database}'
 )
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# Inisialisasi SQLAlchemy
 db = SQLAlchemy(app)
 
 # Model database
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
-
-# Fungsi untuk koneksi langsung menggunakan mysql.connector
-def connect_directly():
-    hostname = "2wxsc.h.filess.io"
-    database = "Responsi0972_rubberdark"
-    port = "3305"
-    username = "Responsi0972_rubberdark"
-    password = "209fd4754cb6b52c80b773bdb355fdaf320010f1"
-
-    try:
-        connection = mysql.connector.connect(
-            host=hostname, database=database, user=username, password=password, port=port
-        )
-        if connection.is_connected():
-            db_info = connection.get_server_info()
-            print("Connected to MariaDB Server version ", db_info)
-            cursor = connection.cursor()
-            cursor.execute("select database();")
-            record = cursor.fetchone()
-            print("You're connected to database: ", record)
-    except Error as e:
-        print("Error while connecting to MariaDB", e)
-    finally:
-        if connection.is_connected():
-            cursor.close()
-            connection.close()
-            print("MariaDB connection is closed")
 
 # Routes
 @app.route('/')
@@ -56,6 +34,8 @@ def index():
 @app.route('/add', methods=['POST'])
 def add():
     title = request.form.get('title')
+    if not title:
+        return redirect(url_for('index'))
     new_task = Task(title=title)
     db.session.add(new_task)
     db.session.commit()
@@ -63,25 +43,44 @@ def add():
 
 @app.route('/delete/<int:id>')
 def delete(id):
-    task = Task.query.get(id)
+    task = Task.query.get_or_404(id)
     db.session.delete(task)
     db.session.commit()
     return redirect(url_for('index'))
 
 @app.route('/update/<int:id>', methods=['POST'])
 def update(id):
-    task = Task.query.get(id)
+    task = Task.query.get_or_404(id)
     task.title = request.form.get('title')
+    if not task.title:
+        return redirect(url_for('index'))
     db.session.commit()
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    # Koneksi langsung ke MariaDB untuk pengujian
-    connect_directly()
+    # Test koneksi ke database menggunakan mysql.connector
+    try:
+        connection = mysql.connector.connect(
+            host=hostname, database=database, user=username, password=password, port=port
+        )
+        if connection.is_connected():
+            db_Info = connection.get_server_info()
+            print("Connected to MariaDB Server version ", db_Info)
+            cursor = connection.cursor()
+            cursor.execute("SELECT DATABASE();")
+            record = cursor.fetchone()
+            print("You're connected to database: ", record)
+    except Error as e:
+        print("Error while connecting to MariaDB", e)
+    finally:
+        if 'connection' in locals() and connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("MariaDB connection is closed")
 
-    # Inisialisasi tabel database melalui SQLAlchemy
+    # Membuat tabel jika belum ada
     with app.app_context():
         db.create_all()
 
-    # Jalankan aplikasi Flask
+    # Menjalankan aplikasi Flask
     app.run(debug=True)
